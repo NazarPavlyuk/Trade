b3b8484253f9dcf75aeb85e37ff38b60d32dd31d
NazarPavlyuk <nazar.pavlyuk@gmail.com>
2016-07-01 14:30:20 +0300

    Added Firebase.Auth

---
+++ app/src/main/java/com/mybringback/thebasics/trade/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.mybringback.thebasics.trade;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ALLO on 01.07.2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+}
